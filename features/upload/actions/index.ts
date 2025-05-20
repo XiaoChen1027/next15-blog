e509a9c0fb6c2a4414b93f02bbe6323d992f4b18
Sakura <2820934123@qq.com>
2025-05-20 14:43:52 +0800

    feat: 添加OSS_BASE_URL配置并更新ali-oss依赖

--- features/upload/actions/index.ts
+++ features/upload/actions/index.ts
@@ -104,14 +104,7 @@ const uploadToOSS = async (input: string) => {
     `${OSS_UPLOAD_DIR}/${fileName}`,
     Buffer.from(buffer),
   );
-  let url = aliOSS.generateObjectUrl(name);
-  if (url) {
-    // 阿里云 OSS 上传后返回的链接是默认是http协议的（但实际上它是也支持https），这里手动替换成https
-    // 因为线上环境网站是使用https协议的，网站里面所有的链接/请求都应该走https（最佳实践是这样）
-    // 要不然浏览器搜索栏会有个小感叹号，不太好看
-    url = url.replace(/http:\/\//g, "https://");
-  }
-  return url;
+  return aliOSS.getObjectUrl(name);
 };
 
 export const uploadFile = async (
--- lib/ali-oss.ts
+++ lib/ali-oss.ts
@@ -4,6 +4,7 @@ import {
   NODE_ENV,
   OSS_ACCESS_KEY_ID,
   OSS_ACCESS_KEY_SECRET,
+  OSS_BASE_URL,
   OSS_BUCKET,
   OSS_REGION,
 } from "@/config";
@@ -13,10 +14,13 @@ const globalForAliOSS = global as unknown as { aliOSS: OSS | undefined };
 export const aliOSS =
   globalForAliOSS.aliOSS ??
   new OSS({
+    endpoint: OSS_BASE_URL,
     accessKeyId: OSS_ACCESS_KEY_ID ?? "",
     accessKeySecret: OSS_ACCESS_KEY_SECRET ?? "",
     region: OSS_REGION ?? "",
     bucket: OSS_BUCKET ?? "",
+    cname: true,
+    secure: true,
   });
 
 if (NODE_ENV !== "production") globalForAliOSS.aliOSS = aliOSS;
